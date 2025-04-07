365af366f108204f566668b1317737b41353de1d
asadnoushad <asad62738473@gmail.com>
2025-04-07 12:28:26 +0530

    Add files via upload

---
+++ basic calculator.py
@@ -0,0 +1,12 @@
+#basic calculator
+a=int(input("enter the first number : "))
+b=int(input("enter the second number : "))
+sum=a+b
+difference=a-b
+product=a*b
+division=a/b
+print("sum : ",sum)
+print("difference : ",difference)
+print("product : ",product)
+print("division : ",division)
+
